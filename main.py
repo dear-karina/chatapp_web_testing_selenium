8f2537ce5b8875c7643d315b7d4983fc6f8cccec
khavy <khavy.january@gmail.com>
2024-03-04 19:14:27 +0700

    configure chrome options 1

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def initialize_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("window-size=1400,1500")
-    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("enable-automation")
-    chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.binary_location = '/usr/bin/google-chrome'
 
