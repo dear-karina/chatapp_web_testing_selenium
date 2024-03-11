7b5bf2760ebb444873f12b7b120bcc0602886fe6
khavy <khavy.january@gmail.com>
2024-03-11 22:30:49 +0700

    configure binaries paths

--- drivers/chrome_driver.py
+++ drivers/chrome_driver.py
@@ -12,10 +12,10 @@ def initialize_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
 
-    # chrome_binary_path = r'/usr/local/bin/chrome-linux64/chrome'
-    # options.binary_location = chrome_binary_path
-    # driver_path = r'/usr/local/bin/chromedriver'
-    # service = Service(driver_path)
-    # cdriver = webdriver.Chrome(options=options, service=service)
+    chrome_binary_path = r'/usr/local/bin/chrome-linux64/chrome'
+    options.binary_location = chrome_binary_path
+    driver_path = r'/usr/local/bin/chromedriver'
+    service = Service(driver_path)
+    cdriver = webdriver.Chrome(options=options, service=service)
     cdriver = webdriver.Chrome(options=options)
     return cdriver
