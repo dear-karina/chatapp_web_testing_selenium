60cc159e371d3e131903c4af3d556a58ddf9bbc4
khavy <khavy.january@gmail.com>
2024-03-15 17:31:02 +0700

    add junit formatter

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import sys
-from behave.__main__ import main as behave_main
+import subprocess
 
 if __name__ == "__main__":
     output_file = "logging/logging.txt"
 
-    # Redirect console output to the file
     with open(output_file, "w") as f:
-        sys.stdout = f
-        sys.stderr = f
-
-        # Run Behave with the @negative tag
-        behave_main(["--tags=@negative-case", "tests/login"])
+        subprocess.run(["behave", "--tags=@negative-case", "--junit", "--no-skipped",
+                        "tests/login"], stdout=f, stderr=subprocess.STDOUT)
