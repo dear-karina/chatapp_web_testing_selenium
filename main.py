88504858163a8d771a2b146860220a23ca3dca54
khavy <khavy.january@gmail.com>
2024-03-15 17:05:51 +0700

    change logging

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys
 from behave.__main__ import main as behave_main
 
 if __name__ == "__main__":
-    behave_main(["tests/login"])
+    output_file = "logging/logging.txt"
+
+    # Redirect console output to the file
+    with open(output_file, "w") as f:
+        sys.stdout = f
+        sys.stderr = f
+
+        # Run Behave with the @negative tag
+        behave_main(["--tags=@negative-case", "tests/login"])
